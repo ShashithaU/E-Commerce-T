796d947dc6db0910d3efc1cbe83cdb9b99a7979e
harindu <manadul-ec20056@stu.stu.kln.ac.lk>
2024-12-18 10:46:58 +0530

    added bucket sammary and cart

---
+++ src/cart/BucketSummary/BucketSummery.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './BucketSummery.module.css';
+
+interface BucketSummeryProps {
+    children: React.ReactNode;
+}
+
+const BucketSummery: React.FC<BucketSummeryProps> = ({children}) => {
+
+    return (
+        <div className={style.BucketSummeryContainer}>
+            <h3>Bucket Summery</h3>
+            <div className={style.BucketSummery}>
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default BucketSummery;
