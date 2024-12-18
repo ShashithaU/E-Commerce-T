5522dcd5d75dc0557ceee02692c063569c8968f5
thurunuym <marasinghe.ty@gmail.com>
2024-12-18 15:07:59 +0530

    h1 to app.tsx

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -17,7 +17,8 @@ function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-    <CssBaseline/>
+    <CssBaseline/>Hello
+    <h1>thurunu new commit</h1>
     <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
     <Container sx={{ paddingTop: "64px" }}>
       <Outlet/>
